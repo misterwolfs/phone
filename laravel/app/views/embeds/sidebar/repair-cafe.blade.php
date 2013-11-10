e9b60d04e2a4e55f5d3f2246460898ac7d45fb13
Nick Hellemans <NickHellemans93@gmail.com>
2013-11-10 18:57:53 +0100

    repaircafé view

--- laravel/app/views/embeds/sidebar/repair-cafe.blade.php
+++ laravel/app/views/embeds/sidebar/repair-cafe.blade.php
@@ -1,6 +1,30 @@
-<h3> Some info about {{ $name }}  </h3>
+<h4>Repaircafé</h4>
 
-<p> Adress : {{ $adress }} </p>
-<p> Postalcode : {{ $postalcode }} </p>
-<p> Village : {{ $village }} </p>
-<p> Website : <a href='{{ $website }}'> {{ $website }} </a></p>
+<div id="phone-view">
+	<div class="wrapper-phone">
+		<div class="category">Information</div>
+		<table class="one-column">
+			<tr>
+				<td>Name:</td>
+				<td>{{ $name }}</td>
+			</tr>
+			<tr>
+				<td>Address:</td>
+				<td>{{ $adress }}</td>
+			</tr>
+			<tr>
+				<td>Postal:</td>
+				<td>{{ $postalcode }}</td>
+			</tr>
+			<tr>
+				<td>Village:</td>
+				<td>{{ $village }}</td>
+			</tr>
+		</table>
+	</div>
+
+	<div class="wrapper-button">
+		<button onclick="window.location.href='{{ $website }}'" class="btn round green no-icon shadow">Visit website</button>
+	</div>
+
+</div>
